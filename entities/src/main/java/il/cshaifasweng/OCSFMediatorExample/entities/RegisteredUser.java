ddee6d267fbe2ebf58018e19834cd1174714e2ee
omerchernia <omer12899@gmail.com>
2024-07-10 21:33:22 +0300

    Fixed RegisteredUser.java

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/RegisteredUser.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/RegisteredUser.java
@@ -1,18 +1,38 @@
 package il.cshaifasweng.OCSFMediatorExample.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.List;
 
-public class RegisteredUser extends ABS_Person{
+@Entity
+@Table(name = "registered_users")
+public class RegisteredUser extends ABS_Person {
+    @Column(nullable = false)
     String email;
+
+    @Column(nullable = false)
+    private int ticket_counter;
+
+    @OneToMany(mappedBy = "registeredUser")
     List<Complaint> complaints;
+
+    @OneToMany(mappedBy = "owner")
     List<ABS_Purchase> purchases;
 
-    public RegisteredUser(int id, String name, boolean isOnline, String email, List<Complaint> complaints, List<ABS_Purchase> purchases) {
+    public RegisteredUser() {
+    }
+
+    public RegisteredUser(int id, String name, boolean isOnline, String email,int ticket_counter List<Complaint> complaints, List<ABS_Purchase> purchases) {
         super(id, name, isOnline);
         this.email = email;
+        this.ticket_counter = ticket_counter;
         this.complaints = complaints;
         this.purchases = purchases;
     }
+
+    // Getters and setters
 
     public String getEmail() {
         return email;
@@ -20,6 +40,14 @@ public class RegisteredUser extends ABS_Person{
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public int getTicket_counter() {
+        return ticket_counter;
+    }
+
+    public void setTicket_counter(int ticket_counter) {
+        this.ticket_counter = ticket_counter;
     }
 
     public List<Complaint> getComplaints() {
