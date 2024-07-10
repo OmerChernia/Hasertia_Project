18c6d4e375f7ee5b5d9141c77d050111b2e3b98f
omerchernia <omer12899@gmail.com>
2024-07-10 20:59:59 +0300

    Fixed ContentManager.java

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/ContentManager.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/ContentManager.java
@@ -1,4 +1,10 @@
 package il.cshaifasweng.OCSFMediatorExample.entities;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "content_manager")
 public class ContentManager {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 }
