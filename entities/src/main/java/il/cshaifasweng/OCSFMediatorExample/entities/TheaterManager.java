9ca898961e20ee0bf9af6f9d1e748ed3b117422b
omerchernia <omer12899@gmail.com>
2024-07-10 21:48:23 +0300

    Fixed TheaterManager.java

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/TheaterManager.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/TheaterManager.java
@@ -1,10 +1,33 @@
 package il.cshaifasweng.OCSFMediatorExample.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "theater_managers")
 public class TheaterManager {
-    Theater theater;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name = "theater")
+    private Theater theater;
+
+    public TheaterManager() {
+    }
+
+    // Getters and Setters
 
     public TheaterManager(Theater theater) {
         this.theater = theater;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public Theater getTheater() {
